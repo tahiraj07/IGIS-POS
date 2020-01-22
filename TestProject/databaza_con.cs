5a2b4078d8d16501f31999bd8fbe01e837050214
Islam.Tahiraj@fti.edu.al <Islam.Tahiraj@fti.edu.al>
2020-01-22 17:15:52 +0100

    db

---
+++ TestProject/databaza_con.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace TestProject
+{
+    class databaza_con
+    {
+        public string Connection()
+        {
+            string con = @"Data Source=.;Initial Catalog=databaza;Integrated Security=True";
+            return con;
+        }
+    }
+}
